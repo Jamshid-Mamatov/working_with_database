7a596d45d4672f937887c5e9bac936d2768f19ec
Jamshid <mamatovjamshid506@gmail.com>
2021-07-05 08:46:57 +0500

    working with method

--- modelapp/models.py
+++ modelapp/models.py
@@ -3,8 +3,12 @@ from django.db.models.base import Model
 
 class Language(models.Model):
     name=models.CharField(max_length=10)
+    
+    def __str__(self) -> str:
+        return self.name 
 
 class Framework(models.Model):
     name=models.CharField(max_length=10)
     language=models.ForeignKey(Language,on_delete=models.CASCADE)
-    
+    def __str__(self) -> str:
+        return self.name 
--- readme.md
+++ readme.md
@@ -2,3 +2,11 @@
 ----
 # table of framework and language
 ![table framework](photos/lan_frame1.jpg)
+
+## Working moduls
+- ![all() method](photos/all_method.jpg)
+
+- ![filter method](photos/filter_method.jpg)
+- ![name](photos/name_startswith.jpg)
+- ![relation](photos/lan_frame_relation.jpg)
+
